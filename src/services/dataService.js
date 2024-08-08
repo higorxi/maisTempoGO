4d0a31b42d328f8eda4c1af04b563107d70522d1
Victor <victoorres@icloud.com>
2024-08-08 02:10:16 -0300

    fix: comentando requisições de orgaos

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -2,18 +2,26 @@ import axios from 'axios';
 import { fetchToken } from './authService';
 
 export const getOrgaos = async () => {
-    try {
-        const token = await fetchToken();
+    // try {
+    //     const token = await fetchToken();
 
-        const response = await axios.get('https://api.go.gov.br/sigac/atendimentos/v1.0/agendamento/listarCondominos', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+    //     const response = await fetch('https://api.go.gov.br/sigac/atendimentos/v1.0/agendamento/listarSenhas', {
+    //         method: 'GET',
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Expose-Headers': 'Authorization', // Adicione cabeçalhos expostos se necessário
+    //             'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept, Origin, Authorization'
+    //         },
+    //     });
 
-        return res.status(response.status).json(response.data);
-    } catch (error) {
-        res.status(error.response?.status || 500).json({ error: error.message });
-        throw error;
-    }
+    //     if (!response.ok) {
+    //         throw new Error(`HTTP error! Status: ${response.status}`);
+    //     }
+
+    //     const data = await response.json();
+    //     res.status(200).json(data);
+    // } catch (error) {
+    //     res.status(500).json({ error: 'Erro ao obter dados dos condomínios', details: error.message });
+    // }
 };
--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -1,13 +1,10 @@
-import cors from '../../lib/cors';
 import { getOrgaos } from '../../services/dataService';
 
 export default async function handler(req, res) {
-    await cors(req, res);
-
-  try {
-    const condominos = await getOrgaos();
-    res.status(200).json(condominos);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao obter dados dos condomínios' });
-  }
+    try {
+        const condominos = await getOrgaos();
+        res.status(200).json(condominos);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao obter dados dos condomínios' });
+    }
 }
